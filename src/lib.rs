a75e4f184edec86f4fc255c3f307bdfbf99bb527
brandon phelps <bwp44f@mst.edu>
2020-12-03 19:01:54 -0600

    fixed test thing

--- src/lib.rs
+++ src/lib.rs
@@ -12,7 +12,7 @@ mod tests {
     #[test]
     fn test_game_init() {
 	let game_state = asteroids::game_init();
-	let game_input = GameInput {
+	let game_input = asteroids::GameInput {
 	    rotation: 0.5,
 	    shoot: true,
 	    thrusters: false,
