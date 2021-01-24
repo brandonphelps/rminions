699c13faf9f0bae06ca44bda4d6fe8c78c232cb4
brandon phelps <bwp44f@mst.edu>
2021-01-23 20:59:05 -0600

    added in some defaults and base container for components

--- src/game_state.rs
+++ src/game_state.rs
@@ -1,19 +1,26 @@
-
 use std::collections::HashSet;
 
 //
 // visual width, visual height
 // meter width, meter height
 
+// Components
+/// Position component, tracks the x, y of an entity. 
+#[derive(Default)]
 pub struct Position {
     x: u32,
     y: u32,
 }
 
+/// Collision component, tracks if the the entity should collide.
+/// Collision only occurs if both entity have collection.
+#[derive(Default)]
 pub struct Collision {
     value: bool,
 }
 
+
+#[derive(PartialEq, Copy, Clone)]
 pub struct Entity(u64);
 
 pub struct EntityManager {
@@ -23,7 +30,8 @@ pub struct EntityManager {
 
 impl EntityManager {
     pub fn new() -> EntityManager {
-	EntityManager { current_entity_id: Entity(0),
+	// purposely starting from 1. 
+	EntityManager { current_entity_id: Entity(1),
 			entities: HashSet::new() }
     }
 
@@ -32,6 +40,50 @@ impl EntityManager {
 	return new_id;
     }
 }
+
+pub struct ComponentManager<T> {
+    components: Vec<T>,
+    entities: Vec<Entity>
+}
+
+impl<T> ComponentManager<T> where T: Default
+{
+    fn new() -> ComponentManager<T> {
+	ComponentManager {
+	    components: Vec::new(),
+	    entities: Vec::new()
+	}
+    }
+
+    /// Checks if an the associated entity contains the component of type T
+    fn contains(&self, entity: &Entity) -> bool {
+	match self.entities.iter().position(|x| x.0 == entity.0) {
+	    Some(_) => true,
+	    None => false,
+	}
+    }
+
+    /// Creates a component of type T and associates it to the entity 
+    fn create(&mut self, entity: &Entity) -> &mut T {
+	if self.contains(entity) {
+	    todo!();
+	}
+	
+	// T must define a default value. 
+	self.components.push(T::default());
+	self.entities.push(*entity);
+
+	let size = self.components.len()-1;
+	return &mut self.components[size];
+    }
+
+    /// Returns the associated component for the entity provided.
+    /// Returns None if the entity does not have such a component. 
+    fn get(&self, entity: Entity) -> Option<&T> {
+	todo!();
+    }
+}
+    
 
 // details a single tile aspect,
 // is the "flooring" that items can stand on.
@@ -61,6 +113,31 @@ mod tests {
     #[test]
     fn entity_manager() {
 	let p = EntityManager::new();
-	assert_eq!(p.current_entity_id.0, 0);
+	assert_eq!(p.current_entity_id.0, 1);
+    }
+
+    #[test]
+    fn components() {
+	let position_component_manager = ComponentManager::<Position>::new();
+	let p = EntityManager::new();
+	let new_e = p.create();
+	assert_eq!(new_e.0, 1);
+
+	assert_eq!(position_component_manager.contains(&new_e), false);
+    }
+
+    #[test]
+    fn components_create() {
+	let mut position_component_manager = ComponentManager::<Position>::new();
+	let p = EntityManager::new();
+	let new_e = p.create();
+	assert_eq!(new_e.0, 1);
+
+	{
+	    let pos = position_component_manager.create(&new_e);
+	    pos.x = 10;
+	    pos.y = 20;
+	}
+	assert_eq!(position_component_manager.contains(&new_e), true);
     }
 }
